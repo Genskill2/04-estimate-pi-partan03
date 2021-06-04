473936d4c622db9e668215b6735ce9b39873b354
partan03 <84659537+partan03@users.noreply.github.com>
2021-06-04 15:00:33 +0530

    soln.2

--- wallis.c
+++ wallis.c
@@ -24,15 +24,12 @@ int main(void) {
   }
 }
 
- float wallis_pi(int n)
-  { 
-     float p=1.0;
-     for (int i=1; i<n+1; i++){
-     
-            p=p*((float)(4*i*i)/(4*i*i-1));
-  }
+  float wallis_pi(int n){
+  float p;
+  int i;
   
-  return 2*p;
-  }
+  for (i=1;i<n;i++)
+  p=(float) ((4*n*n)/4*n*n-1);
   
+  return p=2*p;
   
